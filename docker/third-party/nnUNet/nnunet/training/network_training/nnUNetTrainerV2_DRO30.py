096382cab65bed6ce97f439993b8fc02b711ace6
Lucas Fidon <lucas.fidon@kcl.ac.uk>
2023-07-24 18:10:50 +0100

    removed files not used in nnUNet

--- docker/third-party/nnUNet/nnunet/training/network_training/nnUNetTrainerV2_DRO30.py
+++
@@ -1,9 +0,0 @@
-from nnunet.training.network_training.nnUNetTrainerV2_DRO import nnUNetTrainerV2_DRO
-
-
-class nnUNetTrainerV2_DRO30(nnUNetTrainerV2_DRO):
-    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None,
-                 batch_dice=True, stage=None, unpack_data=True, deterministic=True, fp16=False):
-        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
-                         unpack_data, deterministic, fp16)
-        self.beta = 30
--- docker/third-party/nnUNet/nnunet/training/network_training/nnUNetTrainerV2_RAdam_lr1en3.py
+++
@@ -1,8 +0,0 @@
-from nnunet.training.network_training.nnUNetTrainerV2_RAdam import nnUNetTrainerV2_RAdam
-
-class nnUNetTrainerV2_RAdam_lr1en3(nnUNetTrainerV2_RAdam):
-    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
-                 unpack_data=True, deterministic=True, fp16=False):
-        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
-                         deterministic, fp16)
-        self.initial_lr = 1e-3  # default is 0.01
